d68d4e69f0398c48be63af5da2050028078eb560
Sevgi <sevgi.elesgerova@gmail.com>
2026-08-07 15:55:29 +0400

    bff home dto's created

---
+++ src/Core/SilentMoon.Application/DTOs/Home/HomeSectionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentMoon.Application.DTOs.Home
+{
+    public class HomeSectionDto
+    {
+        public string Title { get; set; }
+        public ICollection<CourseItemDto> CourseItems { get; set; }
+    }
+}
