3966a633055859cee0940a6b1b91697368b185d0
Sevgi <sevgi.elesgerova@gmail.com>
2026-08-11 15:15:34 +0400

    Istorageservice updated

--- src/Core/SilentMoon.Application/Interfaces/Services/IStorageService.cs
+++ src/Core/SilentMoon.Application/Interfaces/Services/IStorageService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace SilentMoon.Application.Interfaces.Services
 {
     public interface IStorageService
     {
-        Task UpdateFileAsync(string file);
-        Task GetFileAsync();
-        Task DownloadFileAsync();
-        Task GetUrl();
+        Task<string> UploadFileAsync(Stream file, string fileName, string contentType);
+
+        Task UpdateFileAsync(Stream file, string fileName, string contentType);
+
+        Task<Stream> GetFileAsync(string fileName);
+
+        Task<byte[]> DownloadFileAsync(string fileName);
+
+        Task<string> GetUrlAsync(string fileName);
+
+        Task DeleteFileAsync(string fileName);
     }
 }
