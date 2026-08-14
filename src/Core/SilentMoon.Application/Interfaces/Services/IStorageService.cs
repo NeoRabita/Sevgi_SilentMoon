528c5d8c9a2c968dbab01e6f96defaa2d706bf92
Sevgi <sevgi.elesgerova@gmail.com>
2026-08-14 12:50:51 +0400

    storage servoce done

--- src/Core/SilentMoon.Application/Interfaces/Services/IStorageService.cs
+++ src/Core/SilentMoon.Application/Interfaces/Services/IStorageService.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace SilentMoon.Application.Interfaces.Services
 {
     public interface IStorageService
     {
-        Task UpdateFileAsync(string file);
-        Task GetFileAsync();
-        Task DownloadFileAsync();
-        Task GetUrl();
+        public Task<string> UploadFileAsync(IFormFile file);
+        public Task<Stream> DownloadFileAsync(string fileName);
+        public string GetUrl(string fileName);
     }
 }
--- src/Infrastructure/SilentMoon.Infrastructure.Persistence/Services/StorageService.cs
+++ src/Infrastructure/SilentMoon.Infrastructure.Persistence/Services/StorageService.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
 using SilentMoon.Application.Interfaces.Services;
 using SilentMoon.Infrastructure.Persistence.Settings;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,31 +15,55 @@ namespace SilentMoon.Infrastructure.Persistence.Services
 {
     public class StorageService : IStorageService
     {
-        private readonly APIAppSettings _settings;
+        private readonly MinioSettings _settings;
+        private readonly IMinioClient _minioClient;
 
-        public StorageService(IOptions<APIAppSettings> settings)
+        public StorageService(IOptions<APIAppSettings> settings, IMinioClient minioClient)
         {
-            _settings = settings.Value;
+            _settings = settings.Value.MinioSettings;
+            _minioClient = minioClient;
         }
 
-        public Task DownloadFileAsync()
+        public async Task<Stream> DownloadFileAsync(string fileName)
         {
-            throw new NotImplementedException();
+            var memoryStream = new MemoryStream();
+            var getObjectArgs = new GetObjectArgs()
+            .WithBucket(_settings.BucketName)
+            .WithObject(fileName)
+            .WithCallbackStream((stream) =>
+            {
+                stream.CopyTo(memoryStream);
+            });
+            await _minioClient.GetObjectAsync(getObjectArgs);
+            memoryStream.Position = 0;
+            return memoryStream;
         }
 
-        public Task GetFileAsync()
+        public async Task<string> UploadFileAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            var bucketExistArgs = new BucketExistsArgs().WithBucket(_settings.BucketName);
+            bool found = await _minioClient.BucketExistsAsync(bucketExistArgs);
+            if (!found)
+            {
+                var makeBucketArgs = new MakeBucketArgs().WithBucket(_settings.BucketName);
+                await _minioClient.MakeBucketAsync(makeBucketArgs);
+            }
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            using var stream = file.OpenReadStream();
+            var putObjectArgs = new PutObjectArgs()
+       .WithBucket(_settings.BucketName)
+       .WithObject(fileName)
+       .WithStreamData(stream)
+       .WithObjectSize(file.Length)
+       .WithContentType(file.ContentType);
+            await _minioClient.PutObjectAsync(putObjectArgs);
+            return fileName;
         }
 
-        public Task GetUrl()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task UpdateFileAsync(string file)
+        public string GetUrl(string fileName)
         {
-            throw new NotImplementedException();
+            return $"http://{_settings.Endpoint}/{_settings.BucketName}/{fileName}";
         }
     }
 }
